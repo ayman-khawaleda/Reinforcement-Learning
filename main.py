4a32dd2824a3bdca46dfb7b062380e0e03dce876
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-12-19 16:42:45 +0300

    Test Qlearning and sarsa

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from Algorithm import Qlearning
+from Algorithm import SARSA, Qlearning
 from Environment import GridEnvironment
 from Agent import GridAgent
-
+import matplotlib.pyplot as plt
 if __name__ == "__main__":
     agent = GridAgent()
     env = GridEnvironment(agent)
-    q_learning = Qlearning(env, total_episodes=500, max_steps=25)
-    print("Start Learning")
+    q_learning = Qlearning(env, total_episodes=500)
     q_learning.fit()
-    q_learning.print_q_table()
+    # q_learning.print_q_table()
+    sarsa = SARSA(env,total_episodes=500)
+    sarsa.fit()
+    # sarsa.print_q_table()
     q_learning.plot_reward()
+    sarsa.plot_reward()
+    plt.show()
+    
