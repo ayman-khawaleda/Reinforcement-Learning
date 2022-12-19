caee654834b42d43a13a910a40e5126edc4c8aed
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-12-19 12:17:17 +0300

    Abstract RL Algorithm Done

---
+++ Algorithm.py
@@ -0,0 +1,30 @@
+from abc import abstractmethod, ABC
+import numpy as np
+from Environment import GridEnvironment,Environment
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+
+class RLAlgorithm(ABC):
+    def __init__(self, env: Environment, total_episodes=10000, lr=0.5, gamma=0.9, epsilon=0.9, decay=0.99, min_epsilon=0.1):
+        self.env = env
+        self.total_episodes = total_episodes
+        self.lr = lr
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.decay = decay
+        self.min_epsilon = min_epsilon
+
+    @abstractmethod
+    def fit(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def policy(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def value_function(self):
+        pass
+
+
