284100a7758424d2d8f017673c50fcdba0ff25a1
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-12-20 12:15:58 +0300

    Plot Env,Paths For Sarsa And Q-learning

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     agent = GridAgent()
     env = GridEnvironment(agent)
+    env.plot_env()
     q_learning = Qlearning(env, total_episodes=500)
     q_learning.fit()
-    env.print_path_as_heatmap(q_learning.value_function,q_learning.total_iters)
     # q_learning.print_q_table()
     sarsa = SARSA(env,total_episodes=500)
     sarsa.fit()
     # sarsa.print_q_table()
     # q_learning.plot_reward()
     # sarsa.plot_reward()
+    env.plot_path_as_heatmap(q_learning.value_function,q_learning.total_iters,"Q-learning")
+    env.plot_path_as_heatmap(sarsa.value_function,sarsa.total_iters,"Sarsa")
     plt.show()
     
