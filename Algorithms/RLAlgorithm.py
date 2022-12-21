198c783c45ad910e0e2a25fe0d7c6fa6d7aa3e46
aymanKH9991 <ayman.khawaleda@gmail.com>
2022-12-21 12:31:17 +0300

    Remove The Seed Command

--- Algorithms/RLAlgorithm.py
+++ Algorithms/RLAlgorithm.py
@@ -2,8 +2,6 @@ from abc import abstractmethod, ABC
 import numpy as np
 from Environments.Environment import Environment
 
-np.random.seed(15)
-
 class RLAlgorithm(ABC):
     def __init__(self, env: Environment, total_episodes=10000, lr=0.5, gamma=0.9, epsilon=0.9, decay=0.99, min_epsilon=0.1):
         self.env = env
