fd22ca38213ff82669969629d934451331518725
aymanKH9991 <ayman.khawaleda@gmail.com>
2023-01-01 12:36:10 +0300

    Seed for tensorflow

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
 import numpy as np
+import tensorflow as tf
 
-def set_numpy_seed(seed:int=15):
+def set_numpy_seed(seed: int = 15):
     np.random.seed(seed)
+    
+def set_tensorflow_seed(seed: int = 15):
+    tf.random.set_seed(seed)
+
+def set_seeds(seed: int = 15):
+    set_numpy_seed(seed)
+    set_tensorflow_seed(seed)
