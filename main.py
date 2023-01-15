72ae929e8005097beda150887e6416730bc784f9
aymanKH9991 <ayman.khawaleda@gmail.com>
2023-01-15 14:46:38 +0300

    View The Q-Table Changes

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     set_numpy_seed(seed=15)
     agent = GridAgent()
-    env = GridEnvironment(agent,reward_values=[200, -10, -1], rows=4, cols=4, win_state=(3, 3))
+    env = GridEnvironment(agent, reward_values=[
+                          200, -10, -1], rows=4, cols=4, win_state=(3, 3))
     env.random_holes(3)
-    env.plot_env()
-
-    dqn = DQN.DQN(env,epochs=50,neurons_num=[12,36],decay=0.97, batch_size=16,lr=0.1)
-    dqn.fit()
-    dqn.plot_reward(step=1)
+    q_lr = Qlearning.Qlearning(env)
+    q_lr.fit()
+    q_lr.animate_q_table_changes()
     # env.plot_path_as_heatmap(dqn.value_function,dqn.total_iters,"DQN")
     plt.show()
