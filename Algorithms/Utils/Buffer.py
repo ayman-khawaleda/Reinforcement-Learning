4e7e5efc7e1f3aa62c94fa05572983d08ae3f3b0
aymanKH9991 <ayman.khawaleda@gmail.com>
2023-01-17 16:56:05 +0300

    Buffer For DDPG

---
+++ Algorithms/Utils/Buffer.py
@@ -0,0 +1,33 @@
+import numpy as np
+import tensorflow as tf
+
+class Buffer:
+    def __init__(self, buffer_capacity) -> None:
+        self.buffer_capacity = buffer_capacity
+        self.buffer_counter = 0
+
+        self.state_buffer = np.zeros((self.buffer_capacity, self.env.num_states))
+        self.action_buffer = np.zeros((self.buffer_capacity, self.env.num_actions))
+        self.reward_buffer = np.zeros((self.buffer_capacity, 1))
+        self.next_state_buffer = np.zeros((self.buffer_capacity, self.env.num_states))
+
+    def remember(self, obs_tuple):
+        index = self.buffer_counter % self.buffer_capacity
+
+        self.state_buffer[index] = obs_tuple[0]
+        self.action_buffer[index] = obs_tuple[1]
+        self.reward_buffer[index] = obs_tuple[2]
+        self.next_state_buffer[index] = obs_tuple[3]
+
+        self.buffer_counter += 1
+
+    def sample(self):
+        record_range = min(self.buffer_counter, self.buffer_capacity)
+        batch_indices = np.random.choice(record_range, self.batch_size)
+        state_batch = tf.convert_to_tensor(self.state_buffer[batch_indices])
+        action_batch = tf.convert_to_tensor(self.action_buffer[batch_indices])
+        reward_batch = tf.convert_to_tensor(self.reward_buffer[batch_indices])
+        reward_batch = tf.cast(reward_batch, dtype=tf.float32)
+        next_state_batch = tf.convert_to_tensor(
+            self.next_state_buffer[batch_indices])
+        return state_batch, action_batch, reward_batch, next_state_batch
